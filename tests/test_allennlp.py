bc5028bd090c0a43014da444f7eeccee6c5a162f
aki <mark@aur.ll.to>
2019-05-03 22:12:05 +0900

    .

---
+++ tests/test_allennlp.py
@@ -0,0 +1,6 @@
+
+def test_allennlp():
+    import allennlp.data.tokenizers
+    tokenizer = allennlp.data.tokenizers.WordTokenizer(start_tokens=['<'], end_tokens=['>'])
+    tokens = tuple([t.text for t in tokenizer.tokenize('Hello World')])
+    assert tokens == ('<', 'Hello', 'World', '>')
