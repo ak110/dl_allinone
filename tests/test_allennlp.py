746a4ae08daf766a952caaadd14101c7f1a91ee3
aki <mark@aur.ll.to>
2020-06-19 14:50:02 +0900

    fix

--- tests/test_allennlp.py
+++ tests/test_allennlp.py
@@ -1,8 +1,6 @@
 def test_run():
     import allennlp.data.tokenizers
 
-    tokenizer = allennlp.data.tokenizers.WordTokenizer(
-        start_tokens=["<"], end_tokens=[">"]
-    )
-    tokens = tuple([t.text for t in tokenizer.tokenize("Hello World")])
-    assert tokens == ("<", "Hello", "World", ">")
+    tokenizer = allennlp.data.tokenizers.WhitespaceTokenizer()
+    tokens = [t.text for t in tokenizer.tokenize("Hello World")]
+    assert tuple(tokens) == ("Hello", "World")
