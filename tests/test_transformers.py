77739968253b636844fc80ba8980bf8245ce9f99
aki <mark@aur.ll.to>
2020-08-18 17:48:18 +0900

    .

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -1,9 +1,8 @@
-
 def test_run():
     import transformers
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(
-        "cl-tohoku/bert-base-japanese-whole-word-masking"
+        "cl-tohoku/bert-base-japanese"
     )
     tokens = tokenizer.tokenize("すもももももももものうち")
     assert tuple(tokens) == ("す", "##も", "##も", "も", "もも", "も", "もも", "の", "うち")
